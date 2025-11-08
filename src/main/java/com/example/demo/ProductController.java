07d18b227b18c671ae23e2c29cae0f1fc194674a
Sathish Kumar Thiyagarajan <sathishk@users.noreply.github.com>
2025-11-08 09:23:23 +0530

    Inject app title and caption into index method
    Added application title and caption attributes to the model.

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+// @Controller
 public class ProductController {
 
+    @Value("${spring.application.name}")
+    private String appTitle;
+
+    @Value("${spring.application.caption}")
+    private String appCaption;
+
     @RequestMapping("/")
-    String index() {
+    String index(final Model model) {
+
+        // th:text="${appTitle}"
+        model.addAttribute("appTitle", appTitle);
+
+        //th:text="${appCaption}"
+        model.addAttribute("appCaption", appCaption);
+
+        // th:each="product, iterStat : ${products}"
+        model.addAttribute("products", ProductLoader.loadProducts());
+
+        // <!--/*
+        // */-->
+
         return "index";
     }
 
